020f1c033693e83512d10087cf91498e9536488e
Brandon Aldrich <brandon@aldr.io>
2020-09-01 19:06:58 -0400

    preview different types of files

--- webapp/src/components/FileConverter/index.tsx
+++ webapp/src/components/FileConverter/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { InputGroup, Text } from 'sancho'
+import { InputGroup, Text, Divider } from 'sancho'
 import styles from './styles'
 import { FileInput } from 'components/FileUpload'
 import prettyBytes from 'pretty-bytes'
@@ -9,11 +9,27 @@ export type FileConverterProps = {}
 export const FileConverter: React.FC<FileConverterProps> = ({}) => {
   const [file, setFile] = useState<File | null>(null)
 
-  const [imageSrc, setImageSrc] = useState<string | null>(null)
+  const [itemPreview, setItemPreview] = useState<JSX.Element | null>(null)
   useEffect(() => {
     if (file) {
       // TODO: verify it is an image
-      setImageSrc(URL.createObjectURL(file))
+      const url = URL.createObjectURL(file)
+      let preview = null
+      if (file.type.startsWith('image/')) {
+        preview = <img src={url} css={{ width: '100%', height: '100%' }} />
+      } else if (file.type.startsWith('video/')) {
+        preview = (
+          <video
+            src={url}
+            css={{ width: '100%', height: '100%' }}
+            autoPlay
+            muted
+            controls
+          />
+        )
+      }
+
+      setItemPreview(preview)
     }
   }, [file])
 
@@ -23,21 +39,22 @@ export const FileConverter: React.FC<FileConverterProps> = ({}) => {
         <FileInput onDropAccepted={(files) => setFile(files[0])} />
 
         {file && (
-          <div css={styles.fileInfo}>
-            {imageSrc && (
+          <>
+            <Divider />
+            <div css={styles.fileInfo}>
+              {itemPreview && (
+                <div css={styles.fileInfoItem}>{itemPreview}</div>
+              )}
               <div css={styles.fileInfoItem}>
-                <img src={imageSrc} css={{ width: '100%', height: '100%' }} />
+                <Text variant="uppercase">Type</Text>
+                <Text>{file.type}</Text>
               </div>
-            )}
-            <div css={styles.fileInfoItem}>
-              <Text variant="uppercase">Type</Text>
-              <Text>{file.type}</Text>
+              <div css={styles.fileInfoItem}>
+                <Text variant="uppercase">Size</Text>
+                <Text>{prettyBytes(file.size)}</Text>
+              </div>
             </div>
-            <div css={styles.fileInfoItem}>
-              <Text variant="uppercase">Size</Text>
-              <Text>{prettyBytes(file.size)}</Text>
-            </div>
-          </div>
+          </>
         )}
       </InputGroup>
       <InputGroup label="To" css={styles.fileColumn}></InputGroup>
